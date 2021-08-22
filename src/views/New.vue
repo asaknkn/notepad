994fefa11b4a4574e2863ecb1541bf53f5a576cb
Kentarou Asai <asaknkn@gmail.com>
2021-08-22 11:48:26 +0900

    create new

---
+++ src/views/New.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="editor">
+    <h1>New Memo</h1>
+    <textarea name="memo" v-model="memoBody"></textarea>
+    <button @click="save">保存</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class New extends Vue {
+  memoBody = "メモです";
+
+  save(): void {
+    alert('保存されました');
+    console.log("test");
+    this.$router.push("/");
+  }
+
+}
+</script>
+
+<style scoped>
+textarea {
+  width: 100%;
+  height: 10em;
+}
+button {
+  border: 1px solid #333;
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+  margin-top: 10px;
+}
+</style>
